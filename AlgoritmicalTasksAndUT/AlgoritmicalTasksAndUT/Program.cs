085290036cf74d6876ef43bc92a9593400358a58
Yana <kolobaeva1390@gmail.com>
2021-06-17 13:47:57 +0200

    init comit

---
+++ AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmicalTasksAndUT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
