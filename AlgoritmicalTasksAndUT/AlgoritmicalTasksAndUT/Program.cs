9e883adbe35a07643b1874950d3d4406de72181b
Yana <kolobaeva1390@gmail.com>
2021-06-17 14:08:44 +0200

    hello console

--- AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/Program.cs
+++ AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/Program.cs
@@ -7,6 +7,7 @@ namespace AlgoritmicalTasksAndUT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Hello");
         }
     }
 }
