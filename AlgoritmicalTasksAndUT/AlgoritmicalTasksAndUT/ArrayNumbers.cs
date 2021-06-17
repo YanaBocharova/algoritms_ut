133b40b74d857039fe055a113087b77de11a5d3e
Yana <kolobaeva1390@gmail.com>
2021-06-17 16:44:29 +0200

    first realization function

---
+++ AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/ArrayNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AlgoritmicalTasksAndUT
+{
+    public class ArrayNumbers
+    {
+
+        public int ReturnNumber(int[]  arr)
+        {
+            Dictionary<int, int> dictNums = new Dictionary<int, int>();
+            int foundValue = -1;
+            if (arr != null && arr.Length!=0)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (dictNums.ContainsKey(arr[i]))
+                    {
+                        dictNums[arr[i]]++;
+                    }
+                    else
+                    {
+                        dictNums[arr[i]] = 1;
+                    }
+                   
+                }
+                int maxNumber = dictNums.Values.Max();
+
+                foreach(var key in dictNums.Keys)
+                {
+                    if (dictNums[key] == maxNumber)
+                    {
+                        foundValue = key;
+                    }
+                }
+                
+            }
+            return foundValue;
+        }
+    }
+}
--- AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/Program.cs
+++ AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/Program.cs
@@ -7,7 +7,23 @@ namespace AlgoritmicalTasksAndUT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Write("Hello");
+
+
+
+
+            int[] arr = { 1, 2, 2, 6, 6, 6, 6, 7, 10};
+            int[] arr2=null;
+
+            ArrayNumbers array = new ArrayNumbers();
+
+            int foundNumber=array.ReturnNumber(arr);
+
+            Console.WriteLine($"found integer= {foundNumber}");
+
+
+
+
+            Console.Read();
         }
     }
 }
