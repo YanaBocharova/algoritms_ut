4ab198f186b99346e8785861072dd958be5fa113
Yana <kolobaeva1390@gmail.com>
2021-06-25 12:48:19 +0300

    created new branch , add class ShowSelfDividingNumbersList and tests

---
+++ AlgoritmicalTasksAndUT/AlgoritmicalTasksAndUT/SelfDividingNumbersList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AlgoritmicalTasksAndUT
+{
+    public class SelfDividingNumbersList
+    {
+        public List<int> SeachSelfDivisibleNumbers(int leftNum, int rigthNum)
+        {
+            List<int> listAllNums = SeachNumber(leftNum, rigthNum);
+
+            if (listAllNums.Count > 0) {
+
+                List<string> numsDivision = listAllNums.Select(item => item.ToString())
+                                                       .Where(item => !item.Contains('0'))
+                                                       .ToList();
+
+                List<string> division = numsDivision.Select(item => item.Trim('-')).ToList();
+
+                List<int> outPutList = new List<int>();
+
+                int tmp = 0;
+
+                bool isDivision = true;
+
+                for (int i = 0; i < numsDivision.Count; i++)
+                {
+                    int.TryParse(numsDivision[i], out tmp);
+
+                    var div = division[i].Select(item => int.Parse(item.ToString())).ToList();
+
+                    isDivision = div.All(item => tmp % item == 0);
+
+                    if (isDivision == true)
+                    {
+                        outPutList.Add(tmp);
+                    }
+                }
+
+                return outPutList;
+            }
+
+            return listAllNums;
+        }
+
+        public List<int> SeachNumber(int leftNum, int rigthNum)
+        {
+            List<int> listNums = new List<int>();
+
+            if (rigthNum > leftNum)
+            {
+                for (int i = leftNum; i <= rigthNum; i++)
+                {
+                    listNums.Add(i);
+                }
+            }
+            if (rigthNum < leftNum)
+            {
+                for (int i = rigthNum; i <= leftNum; i++)
+                {
+                    listNums.Add(i);
+                }
+            }
+
+            return listNums;
+        }
+    }
+}
---
+++ AlgoritmicalTasksAndUT/AlgoritmicalTasksTests/SelfDividingNumbersListTest.cs
@@ -0,0 +1,66 @@
+﻿using AlgoritmicalTasksAndUT;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoritmicalTasksTests
+{
+    [TestClass]
+    public class SelfDividingNumbersListTest
+    {
+        [TestMethod]
+        public void SelfDividingNumbersListTest_InputZeroValue_ReturnEmptyList()
+        {
+            // arrange 
+            var ListNumTestObj = new SelfDividingNumbersList();
+            
+            int left = 0;
+            int right = 0;
+
+            List<int> outPutList = new List<int> { };
+
+            // act
+            var result = ListNumTestObj.SeachSelfDivisibleNumbers(left, right);
+
+            // assert
+            CollectionAssert.AreEqual(outPutList, result);
+        }
+
+        [TestMethod]
+        public void SelfDividingNumbersListTest_InputValues_ReturnResultList()
+        {
+            // arrange 
+            var ArrayNumTestObj = new SelfDividingNumbersList();
+
+            int left = 120;
+            int right = 128;
+
+            List<int> outPutList = new List<int> { 122, 124, 126, 128 };
+
+            // act
+            var result = ArrayNumTestObj.SeachSelfDivisibleNumbers(left, right);
+
+            // assert
+            CollectionAssert.AreEqual(outPutList, result);
+        }
+
+        [TestMethod]
+        public void SelfDividingNumbersListTest_InputNegativeNumbers_ReturnResultList()
+        {
+            // arrange 
+            var ArrayNumTestObj = new SelfDividingNumbersList();
+
+            int left = -10;
+            int right = 15;
+
+            List<int> outPutList = new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15 };
+
+            // act
+            var result = ArrayNumTestObj.SeachSelfDivisibleNumbers(left, right);
+
+            // assert
+            CollectionAssert.AreEqual(outPutList, result);
+        }
+    }
+}
